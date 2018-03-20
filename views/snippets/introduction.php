0f4ce4791b0d55f54e3e386e303435bff6c9c530
Ventricule <pierretandille@gmail.com>
2018-03-20 17:50:21 +0100

    New translations

--- views/snippets/introduction.php
+++ views/snippets/introduction.php
@@ -4,7 +4,7 @@
     <?= l("help") ?>
     <hr>
     <div class="colophon">
-      <div><?= l('about') ?></div>
+      <div><?= l('about') ?> <a href="<?= $libreto->provider('url') ?>" target="_blank"><?= $libreto->provider('name') ?></a></div>
       <div><?= l('credits') ?></div>
     </div>
     <button class="switch">×</button>

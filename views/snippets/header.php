76408f5e98a68156afa47996e670d110a1c6106d
ljf <ljf+git@grimaud.me>
2018-11-27 16:46:47 +0100

    [fix] No CDN to avoid referer

--- views/snippets/header.php
+++ views/snippets/header.php
@@ -6,7 +6,7 @@
   <title><?= $libreto->name() ?> - Libreto</title>
   <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/belgica-belgika" type="text/css"/>
   <link rel="stylesheet" href="/libreto/assets/style.css">
-  <script src="/libreto/assets/jquery-3.3.1.min.js"></script>
+  <script src="/libreto/assets/js/jquery-3.3.1.min.js"></script>
   <script type="text/javascript" src="/libreto/assets/script.js"></script>
 </head>
 <body>
